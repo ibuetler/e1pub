a7ae08795ad305e3d20e4dba66c0e01e01191740
Ivan Buetler <ivan.buetler@compass-security.com>
2017-06-30 17:10:45 +0200

    add ubuntu-novnc3

---
+++ docker/ubuntu-novnc1/doit.sh
@@ -0,0 +1,1 @@
+docker build -t "hackinglab/ubuntu-novnc1" .
---
+++ docker/ubuntu-novnc3/image/startup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir -p /var/run/sshd
+
+echo "hacker:compass" | chpasswd
+echo "root:toor" | chpasswd
+chown -R root:root /root
+chown -R hacker:hacker /home/hacker
+
+#mkdir -p /root/.config/pcmanfm/LXDE/
+#cp /usr/share/doro-lxde-wallpapers/desktop-items-0.conf /root/.config/pcmanfm/LXDE/
+
+cd /usr/lib/web && ./run.py > /var/log/web.log 2>&1 &
+nginx -c /etc/nginx/nginx.conf
+exec /bin/tini -- /usr/bin/supervisord -n
