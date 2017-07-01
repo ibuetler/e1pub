c6cba3477a796f0895e819d0e8ce974f6d554561
Ivan Buetler <ivan.buetler@compass-security.com>
2017-07-01 20:20:00 +0200

    test delete vnc_auto.html

--- docker/ubuntu-novnc3/image/startup.sh
+++ docker/ubuntu-novnc3/image/startup.sh
@@ -10,6 +10,8 @@ chown -R hacker:hacker /home/hacker
 #mkdir -p /root/.config/pcmanfm/LXDE/
 #cp /usr/share/doro-lxde-wallpapers/desktop-items-0.conf /root/.config/pcmanfm/LXDE/
 
+rm /usr/lib/noVNC/vnc_auto.html
+
 cd /usr/lib/web && ./run.py > /var/log/web.log 2>&1 &
 nginx -c /etc/nginx/nginx.conf
 exec /bin/tini -- /usr/bin/supervisord -n
