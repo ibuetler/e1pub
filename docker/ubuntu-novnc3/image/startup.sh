d8cccf34fb00e0056f8e60f95122e7abb45a33fd
Ivan Buetler <ivan.buetler@gmail.com>
2017-07-02 15:00:49 +0000

    add compass background wallpaper

--- docker/ubuntu-novnc3/image/startup.sh
+++ docker/ubuntu-novnc3/image/startup.sh
@@ -11,6 +11,8 @@ chown -R hacker:hacker /home/hacker
 #cp /usr/share/doro-lxde-wallpapers/desktop-items-0.conf /root/.config/pcmanfm/LXDE/
 
 rm /usr/lib/noVNC/vnc_auto.html
+cd /usr/lib/noVNC/
+git pull
 
 cd /usr/lib/web && ./run.py > /var/log/web.log 2>&1 &
 nginx -c /etc/nginx/nginx.conf
