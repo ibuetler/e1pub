0c7ab3acf96cf050e977c6b248463963b074d213
Ivan Buetler <ivan.buetler@gmail.com>
2017-07-04 08:53:58 +0000

    add cathodey

---
+++ docker/ubuntu-openbox-novnc-cathode/image/home/hacker/.mozilla/firefox/8bd80bd5.Browser 1/extensions/{4cc4a13b-94a6-7568-370d-5f9de54a9c7f}/defaults/preferences/prefs.js
@@ -0,0 +1,10 @@
+pref('extensions.{4cc4a13b-94a6-7568-370d-5f9de54a9c7f}.removeMemoryCache', true);
+pref('extensions.{4cc4a13b-94a6-7568-370d-5f9de54a9c7f}.removeDiskCache', true);
+pref('extensions.{4cc4a13b-94a6-7568-370d-5f9de54a9c7f}.removeOfflineCache', true);
+pref('extensions.{4cc4a13b-94a6-7568-370d-5f9de54a9c7f}.removeImageCache', true);
+pref('extensions.{4cc4a13b-94a6-7568-370d-5f9de54a9c7f}.showNotification', true);
+pref('extensions.{4cc4a13b-94a6-7568-370d-5f9de54a9c7f}.doAfterClear', 1);
+pref('extensions.{4cc4a13b-94a6-7568-370d-5f9de54a9c7f}.firstRun', true);
+pref('extensions.{4cc4a13b-94a6-7568-370d-5f9de54a9c7f}.firstRun24', true);
+pref('extensions.{4cc4a13b-94a6-7568-370d-5f9de54a9c7f}.autoClearEnable', false);
+pref('extensions.{4cc4a13b-94a6-7568-370d-5f9de54a9c7f}.autoClearPercent', 70);
