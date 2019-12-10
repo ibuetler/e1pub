4dcab8cf2e8b48a3fd67edc1065cd035e4d51d30
Ivan Buetler <ivan.buetler@compass-security.com>
2019-12-10 17:01:40 +0100

    update initial

---
+++ docker/alpine-base/flag-deploy-scripts/deploy-env-flag.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/with-contenv bash
+
+echo "put your commands to deploy the env based flag here"
---
+++ docker/alpine-base/flag-deploy-scripts/deploy-file-flag.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/with-contenv bash
+
+echo "put your commands to deploy the file based flag here"
